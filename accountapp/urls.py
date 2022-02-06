24d812b5bffbee83a2bb0a17fb874de7acfa7f0e
yimsuson <lab10922@gmail.com>
2022-02-06 13:35:21 +0900

    cour 21

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path,include
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
+
 app_name = 'accountapp'
 
 
 # url, 뷰애 선언한 함수 , path 이름 설정
 urlpatterns = [
-    path('hello_world/', hello_world, name = 'hello_world')
+    path('hello_world/', hello_world, name = 'hello_world'),
+    path('create/', AccountCreateView.as_view(), name = 'create'),
 ]
